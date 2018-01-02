d2a0f3ed5213ed19cb41604a4f1189a5d1cacb79
sriram.j <sriram.j@gofrugal.com>
2018-01-02 18:53:26 +0530

    minor changes to routes

--- routes.js
+++ routes.js
@@ -7,24 +7,4 @@ module.exports = function(app){
       OutletController.test(req, res);
     });
 
-    // app.get('/find/:value', function(req, res) {
-    //   SampleController.findData(req, res);
-    // });
-
-    // app.post('/outlet/category/createOutletCategory', function(req, res) {
-    //   OutletController.createOutletCategory(req, res);
-    // });
-
-    // app.get('/outlet/category', function(req, res) {
-    //   OutletController.displayOutletCategories(req, res);
-    // });
-
-    // app.delete('/outlet/category/deleteOutletCategory/:outletCategoryId', function(req, res) {
-    //   OutletController.deleteOutletCategoryByCategoryId(req, res);
-    // });
-
-    // app.post('/outlet/create', function(req, res) {
-    //   OutletController.createOutlet(req, res);
-    // });
-
 }
