791ecb889d3acf7c631d62681791868982b13000
sriram.j <sriram.j@gofrugal.com>
2018-01-02 18:40:44 +0530

    changes done to make application production ready

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+// const routes = require('./routes');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.send('hi'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+/*DB Connection using moongoose*/
+
+// Or using promises
+mongoose.connect('mongodb://admin:admin@ds237707.mlab.com:37707/orp-todo', { useMongoClient: true }, function (err) {
+  if (err) {
+    console.log('Something went wrong while establishing connection with the database: ', err)
+  } else {
+    console.log('***Successful establishment of connection with the server***')
+  }
+});
+
+/*Set port to 3000*/
+app.listen('1337', function () {
+  console.log('Listening to port 1337...');
+});
+
+/*Define routes to handle all incoming routes*/
+// routes(app);
